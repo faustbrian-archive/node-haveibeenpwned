2ab5f971c6240cfa7ca188e0d8ae61098be5ac5e
Brian Faust <hello@basecode.sh>
2021-09-23 09:09:27 +0300

    first commit

---
+++ source/index.ts
@@ -0,0 +1,30 @@
+import { hash as sha1 } from "@faustbrian/node-sha1";
+import fetch from "cross-fetch";
+
+export const pwned = async (password: string): Promise<number> => {
+  const hashedPassword: string = sha1(password).toString("hex");
+
+  const matches: RegExpExecArray | null = new RegExp(
+    `${hashedPassword.slice(5).toUpperCase()}:(\\d+)`,
+  ).exec(
+    await (await fetch(
+      `https://api.pwnedpasswords.com/range/${hashedPassword.slice(0, 5)}`,
+    )).text(),
+  );
+
+  if (matches && matches[1]) {
+    return Number.parseInt(matches[1]);
+  }
+
+  return 0;
+};
+
+export const evaluate = async (password: string): Promise<boolean> => {
+  if (await pwned(password)) {
+    return false;
+  }
+
+  return new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])(?=.{8,})",
+  ).test(password);
+};
